766b1fdcc70371319b239dcd7dae27ed3b7932e3
Shuxin Zhou <ange4121@gmail.com>
2020-02-15 11:33:29 -0800

    Simple Flashcards Lab done

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Shuxin Zhou on 2/15/20.
+//  Copyright © 2020 Shuxin Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var frontLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func didTapOnFlash(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+}
+
